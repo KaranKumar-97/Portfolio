8e1b7d6d22f94969a07013cddb999d4828cebbbc
Karan Kumar <karankumar.msit@gmail.com>
2025-05-08 22:39:54 +0530

    update porjects

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -33,22 +33,22 @@ const Projects = () => {
       github: "https://github.com/KaranKumar-97/gigverse",
       skills: [{title:"React",img:"react.png"},{title:"Zustand",img:"zustand.svg"},{title:"Tailwind CSS",img:"tailwind.svg"},{title:"MUI",img:"mui.png"},{title:"Node.js",img:"node.png"},{title:"Express.js",img:"express.jpg"},{title:"MongoDB",img:"mongo.png"}],
     },
-    {
-      image: "swiftshop.png",
-      title: "SwiftShop",
-      desc: "Developed e-commerce web app using React, Redux Toolkit, and Tailwind CSS, featuring dynamic product display, efficient cart management, and seamless state handling with API integration.",
-      link: "https://react-swift-shop.vercel.app/",
-      github: "https://github.com/KaranKumar-97/SwiftShop",
-      skills: [{title:"React",img:"react.png"},{title:"Redux",img:"redux.png"},{title:"Tailwind CSS",img:"tailwind.svg"}],
-    },
-    {
-      image: "weather.png",
-      title: "Weather Web App",
-      desc: "A Weather Web App using HTML, CSS, and JavaScript with OpenWeatherMap API for real-time weather updates. It shows weather for current location and any city.",
-      liveLink: "https://karankumar-97.github.io/Weather-App/",
-      githubLink: "https://github.com/KaranKumar-97/Weather-App",
-      skills: [{title:"HTML",img:"html.png"},{title:"CSS",img:"css.png"},{title:"JavaScript",img:"js.png"}],
-    },
+    // {
+    //   image: "swiftshop.png",
+    //   title: "SwiftShop",
+    //   desc: "Developed e-commerce web app using React, Redux Toolkit, and Tailwind CSS, featuring dynamic product display, efficient cart management, and seamless state handling with API integration.",
+    //   link: "https://react-swift-shop.vercel.app/",
+    //   github: "https://github.com/KaranKumar-97/SwiftShop",
+    //   skills: [{title:"React",img:"react.png"},{title:"Redux",img:"redux.png"},{title:"Tailwind CSS",img:"tailwind.svg"}],
+    // },
+    // {
+    //   image: "weather.png",
+    //   title: "Weather Web App",
+    //   desc: "A Weather Web App using HTML, CSS, and JavaScript with OpenWeatherMap API for real-time weather updates. It shows weather for current location and any city.",
+    //   liveLink: "https://karankumar-97.github.io/Weather-App/",
+    //   githubLink: "https://github.com/KaranKumar-97/Weather-App",
+    //   skills: [{title:"HTML",img:"html.png"},{title:"CSS",img:"css.png"},{title:"JavaScript",img:"js.png"}],
+    // },
   ];
   return (
     <div className="w-[92%] mx-auto mt-20" id="projects">
